cce7a2c4c771fe33df34a90af473f51e44417e18
Alexandre MODESTO <alexandre.modesto@covage.com>
2020-03-15 14:23:11 +0100

    Première version du script

---
+++ script.sh
@@ -0,0 +1,16 @@
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=albiez-montrond&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Albiez-Montrond.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=aussois&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Aussois.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=bonneval-sur-arc&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Bonneval sur Arc.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=la-norma&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/La Norma.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=la-toussuire&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/La Toussuire.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=le-corbier&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Le Corbier.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=les-karellis&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Les Karellis.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=orelle&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Orelle.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=saint-colomban&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Saint-Colomban.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=saint-francois-longchamp&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Saint-François Longchamp.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=saint-jean&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Saint-Jean d’Arves.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=saint-sorlin&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Saint Sorlin d’Arves.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=sybelles&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Sybelles.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=valloire&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Valloire.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=valfrejus&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Valfréjus.jpg
+/home/alex/node_modules/.bin/webshot 'http://www.skiplan.com/bulletin/bulletin.php?station=valmeinier&region=alpes&pays=france&lang=fr' --window-size=1024/2000 /tmp/temp/Valmeinier.jpg
